ff5331fb82f1148087dceff54a5674fb0080368b
David Inoa <davidinoa@gmail.com>
2018-01-20 00:48:53 -0500

    Create template for a flashcard

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/cards', (req, res) => {
+  res.render('card', {
+    prompt: 'Who is buried in Grant\'s tomb?',
+    hint: 'Think about whose tomb it is'
+  });
+});
+
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log('The application is running on localhost:3002');
