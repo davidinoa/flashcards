9f37c0b61d54ef788961160c5ce9017ae5d283c3
David Inoa <davidinoa@gmail.com>
2018-01-20 00:32:36 -0500

    Add Pug template engine

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 const port = process.env.PORT || 3002;
