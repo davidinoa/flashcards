61ad04687273446fc751199c0172a123825eebd4
David Inoa <davidinoa@gmail.com>
2018-01-28 10:01:36 -0500

    Handle 404 errors

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-  const err = new Error('Oh no!');
-  err.status = 500;
-  next(err);
-});
-
 app.get('/', (req, res) => {
   const name = req.cookies.username;
   if (name) {
@@ -48,6 +42,12 @@ app.post('/hello', (req, res) => {
 app.post('/goodbye', (req, res) => {
   res.clearCookie('username');
   res.redirect('/hello');
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.use((err, req, res, next) => {
