edd218a8cbd027e13fe3d4cb3b93e2db816c1bee
David Inoa <davidinoa@gmail.com>
2018-01-28 10:12:29 -0500

    Modularize app routes

---
+++ routes/cards.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('card', {
+    prompt: 'Who is buried in Grant\'s tomb?',
+    hint: 'Think about whose tomb it is',
+  });
+});
+
+module.exports = router;
