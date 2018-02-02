486212692b578952f063fdf2c8a4365e44433d7e
David Inoa <davidinoa@gmail.com>
2018-02-02 11:20:37 -0500

    Define user schema and model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const db = require('./database/index.js');
 
 const app = express();
 
--- database/index.js
+++ database/index.js
@@ -5,3 +5,5 @@ const db = mongoose.connection;
 
 db.on('error', () => console.log('mongoose connection error'));
 db.once('open', () => console.log('mongoose connected successfully'));
+
+module.exports = db;
